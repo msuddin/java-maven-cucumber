0242cf25b2316d5aabe9cc850917b06bdb021c8a
Mohammed S Uddin <mo@thetestroom.com>
2018-04-22 13:06:37 +0100

    Properties file for trading accounts

--- src/test/java/definition/StepDefinitions.java
+++ src/test/java/definition/StepDefinitions.java
@@ -5,59 +5,106 @@ import cucumber.api.java.After;
 import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
+import org.jboss.aerogear.security.otp.Totp;
+import org.junit.Assert;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.List;
+import java.util.Properties;
+
+import static org.hamcrest.Matchers.is;
+
 public class StepDefinitions {
 
-    WebDriver driver = new ChromeDriver();
+    private WebDriver driver = new ChromeDriver();
+    private Properties prop = new Properties();
 
-    @Given("^I navigate to site$")
-    public void i_navigate_to_site() {
+    @Given("^I navigate to site with user ([^\"]*)$")
+    public void navigateTo(String userFile) throws IOException {
+        InputStream input = getClass().getClassLoader()
+                .getResourceAsStream(userFile + ".properties");
+        prop.load(input);
+
         System.setProperty("webdriver.chrome.driver", "chromedriver.exe");
-        driver.navigate().to("https://office.tradecoinclub.com/login");
+        driver.manage().window().maximize();
+        driver.navigate().to(prop.getProperty("url"));
     }
 
-    @When("^I enter username ([^\"]*)$")
-    public void i_enter_username(String username) {
-        driver.findElement(By.name("login")).sendKeys(username);
+    @When("^I enter username$")
+    public void enterUsername() {
+        waitForElementTobeVisible(By.name("login"));
+        driver.findElement(By.name("login")).sendKeys(prop.getProperty("username"));
     }
 
-    @When("^I enter password ([^\"]*)$")
-    public void i_enter_password(String password) {
-        driver.findElement(By.name("password")).sendKeys(password);
+    @When("^I enter password$")
+    public void enterPassword() {
+        waitForElementTobeVisible(By.name("password"));
+        driver.findElement(By.name("password")).sendKeys(prop.getProperty("password"));
     }
 
     @When("^I press sign in")
-    public void i_press_sign_in() {
+    public void signIn() {
+        waitForElementTobeVisible(By.id("sign-in"));
         driver.findElement(By.id("sign-in")).click();
+        waitForElementTobeVisible(By.id("validateCode"));
     }
 
     @When("^I wait for google authenticator to be completed$")
-    public void i_wait_for_google_authenticator_to_be_completed() {
-        findTextOnPage("Successfully logged in!");
+    public void googleAuthenticator() {
+        waitForElementTobeVisible(By.id("validateCode"));
+        driver.findElement(By.id("validateCode")).sendKeys(generateGoogleAuthenticatorCode());
+
+        waitForElementTobeVisible(By.id("BTNvalidateCode"));
+        driver.findElement(By.id("BTNvalidateCode")).click();
+
+        waitForElementTobeVisible(By.cssSelector("#jquery-notific8-notification-1"));
+        waitForElementTobeVisible(By.id("item-package"));
     }
 
-    @Then("^I turn on trading$")
-    public void i_turn_on_trading() throws Throwable {
+    @Then("^I set trading to \"([^\"]*)\"$")
+    public void setTrading(String tradeSetting) {
         throw new PendingException();
     }
 
-    @Then("^wait for the confirmation$")
-    public void wait_for_the_confirmation() {
-        findTextOnPage("Risk saved and shit!");
+    @Then("^wait for trading confirmation$")
+    public void tradingConfirmation() {
+        throw new PendingException();
     }
 
-    private void findTextOnPage(String text) {
-        WebDriverWait wait = new WebDriverWait(driver, 60);
-        wait.until(ExpectedConditions
-                .visibilityOfElementLocated(
-                        By.xpath("//*[contains(text(), " + text + ")]")
-                )
-        );
+    @Then("^I log out$")
+    public void logOut() {
+        List<WebElement> h2 = driver.findElements(By.tagName("h2"));
+        for (int i = 0; i < h2.size(); i++) {
+            System.out.println(h2.get(i).getText());
+            if (h2.get(i).getText().contains(prop.getProperty("firstName")) &&
+                    h2.get(i).getText().contains(prop.getProperty("surName"))) {
+                h2.get(i).click();
+                break;
+            }
+        }
+
+        waitForElementTobeVisible(By.linkText("Logout"));
+        driver.findElement(By.linkText("Logout")).click();
+
+        Assert.assertThat(driver.getCurrentUrl(), is(prop.getProperty("url")));
+    }
+
+    private String generateGoogleAuthenticatorCode() {
+        String otpKeyStr = prop.getProperty("secret");
+        Totp totp = new Totp(otpKeyStr);
+        return totp.now();
+    }
+
+    private void waitForElementTobeVisible(By element) {
+        WebDriverWait wait = new WebDriverWait(driver, 10);
+        wait.until(ExpectedConditions.visibilityOfElementLocated(element));
     }
 
     @After
--- src/test/java/trading.feature
+++ src/test/java/trading.feature
@@ -1,15 +1,15 @@
 Feature: To turn on trading
 
   Scenario Outline: Turn on trading for all users
-    Given I navigate to site
-    When I enter username <username>
-    And I enter password <password>
+    Given I navigate to site with user <username>
+    When I enter username
+    And I enter password
     And I press sign in
     And I wait for google authenticator to be completed
-    Then I turn on trading
-    And wait for the confirmation
+    #Then I set trading to "some risk"
+    #And wait for trading confirmation
+    And I log out
 
     Examples:
-    |username|password|
-    |missatwo|0Htw3$A%qE0s|
-    |missatwo|0Htw3$A%qE0s|
+    |username|
+    |template|
