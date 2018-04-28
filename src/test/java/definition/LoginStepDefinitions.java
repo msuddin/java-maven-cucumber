f05b673f0c32d8376f8e779b63e104ec2135822a
Mohammed S Uddin <mo@thetestroom.com>
2018-04-28 13:52:46 +0100

    Updated Login Steps to allow login to other mail portals

--- src/test/java/definition/LoginStepDefinitions.java
+++ src/test/java/definition/LoginStepDefinitions.java
@@ -4,6 +4,8 @@ import cucumber.api.java.en.Then;
 import org.junit.Assert;
 import org.openqa.selenium.By;
 import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
 
 import java.util.ArrayList;
 
@@ -14,7 +16,11 @@ public class LoginStepDefinitions extends DefinitionUtils {
     @Then("^I open a new tab for email$")
     public void openNewTab() {
         JavascriptExecutor js = (JavascriptExecutor) driver;
-        js.executeScript("window.open('https://gmail.com','_blank');");
+        if (prop.getProperty("username").equals("mohammedsuddin")) {
+            js.executeScript("window.open('https://secure.ipage.com/secure/login.bml#webmail','_blank');");
+        } else {
+            js.executeScript("window.open('https://gmail.com','_blank');");
+        }
 
         ArrayList<String> tabs = new ArrayList<String> (driver.getWindowHandles());
         driver.switchTo().window(tabs.get(1));
@@ -22,21 +28,42 @@ public class LoginStepDefinitions extends DefinitionUtils {
 
     @Then("^I enter email username$")
     public void enterEmailUsername() {
-        waitForElementTobeVisible(By.id("identifierId"));
-        driver.findElement(By.id("identifierId")).sendKeys(prop.getProperty("emailAdd"));
-        driver.findElement(By.id("identifierNext")).click();
+        if (prop.getProperty("username").equals("mohammedsuddin")) {
+            waitForElementTobeVisible(By.id("address"));
+            driver.findElement(By.id("address")).sendKeys(prop.getProperty("emailAdd"));
+        } else {
+            waitForElementTobeVisible(By.id("identifierId"));
+            driver.findElement(By.id("identifierId")).sendKeys(prop.getProperty("emailAdd"));
+            driver.findElement(By.id("identifierNext")).click();
+        }
     }
 
     @Then("^I enter email password$")
     public void enterEmailPassword() {
         waitForElementTobeVisible(By.name("password"));
         driver.findElement(By.name("password")).sendKeys(prop.getProperty("emailPass"));
-        driver.findElement(By.id("passwordNext")).click();
+        if (prop.getProperty("username").equals("mohammedsuddin")) {
+
+            WebElement element = driver.findElement(By.linkText("Not a customer? Sign up now!"));
+            Actions actions = new Actions(driver);
+            actions.moveToElement(element);
+            actions.perform();
+
+            driver.findElement(By.id("webmailLogin")).click();
+        } else {
+            driver.findElement(By.id("passwordNext")).click();
+        }
     }
 
     @Then("^I confirm I have logged in$")
     public void confirmEmailLogin() {
-        waitForElementTobeVisible(By.id("gba"));
-        Assert.assertThat(driver.getTitle(), containsString(prop.getProperty("emailAdd")));
+        if (prop.getProperty("username").equals("mohammedsuddin")) {
+            waitForElementTobeVisible(By.className("username"));
+            Assert.assertThat(driver.findElement(By.className("username")).getText(),
+                    containsString(prop.getProperty("emailAdd")));
+        } else {
+            waitForElementTobeVisible(By.id("gba"));
+            Assert.assertThat(driver.getTitle(), containsString(prop.getProperty("emailAdd")));
+        }
     }
 }
