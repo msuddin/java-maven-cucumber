dd3326f4161832465db95083674493fec222b093
Mohammed S Uddin <mo@thetestroom.com>
2018-05-09 13:59:53 +0100

    Added check for Yahoo Email

--- src/test/java/definition/LoginStepDefinitions.java
+++ src/test/java/definition/LoginStepDefinitions.java
@@ -18,7 +18,9 @@ public class LoginStepDefinitions extends DefinitionUtils {
         JavascriptExecutor js = (JavascriptExecutor) driver;
         if (prop.getProperty("username").equals("mohammedsuddin")) {
             js.executeScript("window.open('https://secure.ipage.com/secure/login.bml#webmail','_blank');");
-        } else {
+        } else if (prop.getProperty("emailAdd").contains("@yahoo.com")) {
+            js.executeScript("window.open('https://login.yahoo.com','_blank');");
+        }else {
             js.executeScript("window.open('https://gmail.com','_blank');");
         }
 
@@ -31,6 +33,10 @@ public class LoginStepDefinitions extends DefinitionUtils {
         if (prop.getProperty("username").equals("mohammedsuddin")) {
             waitForElementTobeVisible(By.id("address"));
             driver.findElement(By.id("address")).sendKeys(prop.getProperty("emailAdd"));
+        } else if(prop.getProperty("emailAdd").contains("@yahoo.com")) {
+            waitForElementTobeVisible(By.id("login-username"));
+            driver.findElement(By.id("login-username")).sendKeys(prop.getProperty("emailAdd"));
+            driver.findElement(By.id("login-signin")).click();
         } else {
             waitForElementTobeVisible(By.id("identifierId"));
             driver.findElement(By.id("identifierId")).sendKeys(prop.getProperty("emailAdd"));
@@ -43,13 +49,20 @@ public class LoginStepDefinitions extends DefinitionUtils {
         waitForElementTobeVisible(By.name("password"));
         driver.findElement(By.name("password")).sendKeys(prop.getProperty("emailPass"));
         if (prop.getProperty("username").equals("mohammedsuddin")) {
-
             WebElement element = driver.findElement(By.linkText("Not a customer? Sign up now!"));
             Actions actions = new Actions(driver);
             actions.moveToElement(element);
             actions.perform();
-
             driver.findElement(By.id("webmailLogin")).click();
+        } else if (prop.getProperty("emailAdd").contains("@yahoo.com")) {
+            driver.findElement(By.id("login-signin")).click();
+            WebElement element = driver.findElement(By.name("agree"));
+            Actions actions = new Actions(driver);
+            actions.moveToElement(element);
+            actions.perform();
+            driver.findElement(By.name("agree")).click();
+            waitForElementTobeVisible(By.id("uh-mail-link"));
+            driver.findElement(By.id("uh-mail-link")).click();
         } else {
             driver.findElement(By.id("passwordNext")).click();
         }
@@ -61,6 +74,10 @@ public class LoginStepDefinitions extends DefinitionUtils {
             waitForElementTobeVisible(By.className("username"));
             Assert.assertThat(driver.findElement(By.className("username")).getText(),
                     containsString(prop.getProperty("emailAdd")));
+        } else if (prop.getProperty("emailAdd").contains("@yahoo.com")) {
+            waitForElementTobeVisible(By.id("ybar"));
+            Assert.assertThat(driver.findElement(By.id("ybarAccountMenu")).getAttribute("aria-label"),
+                    containsString(prop.getProperty("firstName")));
         } else {
             waitForElementTobeVisible(By.id("gba"));
             Assert.assertThat(driver.getTitle(), containsString(prop.getProperty("emailAdd")));
